fee1263483903a55282db28548d7be8d1ef2955c
alidanish935 <114473927+alidanish935@users.noreply.github.com>
2024-05-14 17:49:01 +0530

    try1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args) {
+    RWOnly r = new RWOnly();
+    //r.name ="danish";
+    r.setName("danish");
+    System.out.println(r.getName());
+  }
 }
